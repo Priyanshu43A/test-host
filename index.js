59de66db7f51d6719aaefd677cd838ef86d02f6d
Priyanshu <jashram826@gmail.com>
2024-08-19 05:26:48 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/greet/:name", (req, res) => {
+  res.render("Home", { name: req.params.name });
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World! i am priyanshu");
+  res.render("create");
 });
 
 const port = process.env.PORT || 3000;
