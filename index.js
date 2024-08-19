98f461ed3bdd1ed641a0dacb76da0500d825a8c6
Priyanshu <jashram826@gmail.com>
2024-08-19 06:12:22 +0530

    final touches

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
-app.get("/greet/:name", (req, res) => {
-  res.render("Home", { name: req.params.name });
-});
 
 app.get("/", (req, res) => {
   res.render("create");
+});
+
+app.get("/greet/:name", (req, res) => {
+  res.render("Home", { name: req.params.name });
 });
 
 const port = process.env.PORT || 3000;
